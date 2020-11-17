0643e6fc592543134aac288c7777d6565d05e005
Yash-Kudesia <yashku2000@gmail.com>
2020-11-17 18:21:27 +0530

    Added Education

---
+++ js/src.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+//   var instance = M.Carousel.getInstance(elem);
+
+  var instance = M.Carousel.init({
+    fullWidth: true
+  });
